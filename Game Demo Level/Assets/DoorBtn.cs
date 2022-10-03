779d08643426fc07bca719a80f69c4b6c79e9c68
Chris Buter <c.buter@tmdi.nl>
2022-10-03 15:50:15 +0200

    Door can now open and close

--- Game Demo Level/Assets/DoorBtn.cs
+++ Game Demo Level/Assets/DoorBtn.cs
@@ -1,9 +1,10 @@
 using Assets;
 using UnityEngine;
-using Debug = UnityEngine.Debug;
 
 public class DoorBtn : Interactable
 {
+    public GameObject Target;
+    private bool interacted = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +19,21 @@ public class DoorBtn : Interactable
     public void HandleInteraction()
     {
         //Logic for interaction
-        Debug.Log("Interaction called");
         var cubeRenderer = transform.GetComponent<Renderer>();
-
-        // Call SetColor using the shader property name "_Color" and setting the color to red
-        cubeRenderer.material.SetColor("_Color", Color.green);
-        Debug.Log("Color set to green");
+        //open door if not already.
+        if (!interacted)
+        {
+            cubeRenderer.material.SetColor("_Color", Color.green);
+            Target.transform.eulerAngles = new Vector3(0, 90, 0);
+            interacted = true;
+        }
+        else
+        {
+            //close door
+            cubeRenderer.material.SetColor("_Color", Color.red);
+            Target.transform.eulerAngles = new Vector3(0, 0, 0);
+            interacted = false;
+        }
     }
     public override void Interact()
     {
